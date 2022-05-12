1c979ef752f3b2f3624425acdd3d946e39b0a8af
HectorIlarraza <53978521+HectorIlarraza@users.noreply.github.com>
2022-05-12 11:55:30 -0500

    adde new user auth and deleted user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,26 +1,47 @@
+// 
 const express = require('express');
 
 const user = express.Router();
 
-const { doesUserExist } = require('../queries/users.js');
+const bcrypt = require("bcrypt");
 
+//
+const { doesUserExist, addUser, deleteUser } = require('../queries/users.js');
+
+// This is our Create User ROUTE≠
 user.post('/', async (req, res) => {
     const { username, email, password } = req.body;
     try {
-        if (!(await doesUserExist(email))) {
+        if ((await doesUserExist(email))) {
             res.status(400).send('User with that email already exists');
             return;
         }
-                
+        const hashPassword = await bcrypt.hash(password, 10);
+        try {
+            await addUser(username, email, hashPassword);
+            res.status(200).send("User has been created!")
+        } catch (err) {
+            res.status(500).send(err, "Failed User creation")
+        }   
     } catch (err) {
         res.status(404).send('Post failed.', err);
     }
 });
 
+user.delete('/:id', async (req, res) => {
+    const {id} = req.params;
+    try{
+        const deletedUser = await deleteUser(id);
+        res.status(200).json(deletedUser);
+    }catch(err){
+        res.status(404).json({error: "User was not found"})
+    }
+});
+
 
 /* REQUIRED ROUTES
-create user
-delete user
+create user *DONE
+delete user *DONE
 authenticate user
 update user
 */
--- backend/queries/users.js
+++ backend/queries/users.js
@@ -1,3 +1,4 @@
+const { as } = require('pg-promise');
 const db = require('../db/dbConfig.js');
 
 const doesUserExist = async (email) => {
@@ -11,9 +12,20 @@ const doesUserExist = async (email) => {
     }
 }
 
-const addUser = async (username, email, password) => {
+const addUser = async (name, email, password) => {
     try {
-        
+        const newUser = await db.one("INSERT INTO users (username, email, password) VALUES ($1,$2,$3) RETURNING username, email", 
+        [name, email, password])
+        return newUser;
+    } catch (err) {
+        return err;
+    }
+}
+
+const deleteUser = async (id) => {
+    try {
+        const deletedUser = await db.one("DELETE FROM users WHERE user_id = $1 RETURNING username", id)
+        return deletedUser;
     } catch (err) {
         return err;
     }
@@ -22,4 +34,6 @@ const addUser = async (username, email, password) => {
 module.exports = 
     {
         doesUserExist,
+        addUser,
+        deleteUser
     }
