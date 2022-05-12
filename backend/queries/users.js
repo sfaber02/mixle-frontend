07f7e149791508f4af5448753fca247f3cce346a
JoshMarte <joshuelm@gmail.com>
2022-05-12 15:12:17 -0400

    should have bcrypt

--- backend/queries/users.js
+++ backend/queries/users.js
@@ -1,6 +1,6 @@
-const { as } = require('pg-promise');
-const db = require('../db/dbConfig.js');
+const db = require("../db/dbConfig.js");
 
+// VALIDATE USER EMAIL
 const doesUserExist = async (email) => {
     try {
         const exists = await db.query(
@@ -10,30 +10,50 @@ const doesUserExist = async (email) => {
     } catch (err) {
         return error;
     }
-}
+};
 
+// SELECT SPECIFIC USER
+const getUser = async (email) => {
+    try {
+        const newUser = await db.one(
+            "SELECT * FROM users WHERE email = $1",
+            email
+        );
+        return newUser;
+    } catch (error) {
+        return error;
+    }
+};
+
+// CREATE A USER
 const addUser = async (name, email, password) => {
     try {
-        const newUser = await db.one("INSERT INTO users (username, email, password) VALUES ($1,$2,$3) RETURNING username, email", 
-        [name, email, password])
+        const newUser = await db.one(
+            "INSERT INTO users (username, email, password) VALUES ($1,$2,$3) RETURNING username, email",
+            [name, email, password]
+        );
         return newUser;
     } catch (err) {
         return err;
     }
-}
+};
 
+// DELETE A USER
 const deleteUser = async (id) => {
     try {
-        const deletedUser = await db.one("DELETE FROM users WHERE user_id = $1 RETURNING username", id)
+        const deletedUser = await db.one(
+            "DELETE FROM users WHERE user_id = $1 RETURNING username",
+            id
+        );
         return deletedUser;
     } catch (err) {
         return err;
     }
-}
+};
 
-module.exports = 
-    {
-        doesUserExist,
-        addUser,
-        deleteUser
-    }
+module.exports = {
+    doesUserExist,
+    addUser,
+    deleteUser,
+    getUser,
+};
