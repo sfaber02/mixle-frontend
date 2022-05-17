3afa6a35604dc2fa7e2cf12ea06ea8ffa9cdbb03
JoshMarte <joshuelm@gmail.com>
2022-05-16 20:14:13 -0400

    some fixes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,12 +5,12 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const {
-  doesUserExist,
-  addUser,
-  deleteUser,
-  getUser,
-  updateUser,
-  getUserById,
+    doesUserExist,
+    addUser,
+    deleteUser,
+    getUser,
+    updateUser,
+    getUserById,
 } = require("../queries/users.js");
 
 // CONFIGURATION
@@ -18,81 +18,82 @@ const user = express.Router();
 
 // REGISTER POST REQUEST
 user.post("/register", async (req, res) => {
-  const { username, email, password } = req.body;
-  // console.log(await doesUserExist(email));
-  try {
-    if (await doesUserExist(email)) {
-      res.status(400).send("User with that email already exists");
-      return;
-    }
-    const hashPassword = await bcrypt.hash(password, 10);
+    const { username, email, password } = req.body;
     try {
-      await addUser(username, email, hashPassword);
-      console.log(addUser);
-      res.status(200).send("User has been created!");
-      const token = jwt.sign({ email: email }, process.env.SECRET_KEY);
+        if (await doesUserExist(email)) {
+            res.status(400).send("User with that email already exists");
+            return;
+        }
+        const hashPassword = await bcrypt.hash(password, 10);
+        try {
+            const user = await addUser(username, email, hashPassword);
+            res.status(200).send("User has been created!");
+            const token = jwt.sign({ email: email }, process.env.SECRET_KEY);
+        } catch (err) {
+            res.status(500).send(err, "Failed User creation");
+        }
     } catch (err) {
-      res.status(500).send(err, "Failed User creation");
+        res.status(404).send("Post failed");
     }
-  } catch (err) {
-    res.status(404).send("Post failed");
-  }
 });
 
 // LOGIN POST REQUEST
 user.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const user = await getUser(email);
-    if (!user.email) {
-      res.status(400).send("Email not found.");
-    } else {
-      const isValidPassword = await bcrypt.compare(password, user.password);
-      if (isValidPassword) {
-        const token = jwt.sign({ email }, process.env.SECRET_KEY);
-        
-        res.status(200).json({
-          message: "USER signed in!",
-          user: user.username,
-          email: user.email,
-          token,
-        });
-      } else {
-        res.status(400).json({
-          error: "Enter correct password!",
-        });
-      }
+    const { email, password } = req.body;
+    try {
+        const user = await getUser(email);
+        if (!user.email) {
+            res.status(400).send("Email not found.");
+        } else {
+            const isValidPassword = await bcrypt.compare(
+                password,
+                user.password
+            );
+            if (isValidPassword) {
+                const token = jwt.sign({ email }, process.env.SECRET_KEY);
+
+                res.status(200).json({
+                    message: "USER signed in!",
+                    user: user.username,
+                    email: user.email,
+                    token,
+                });
+            } else {
+                res.status(400).json({
+                    error: "Enter correct password!",
+                });
+            }
+        }
+    } catch (error) {
+        res.status(404).json({ error: error });
     }
-  } catch (error) {
-    res.status(404).json({ error: error });
-  }
 });
 
 // UPDATE USER
 user.put("/:id", async (req, res) => {
-  const { id } = req.params;
-  const { password } = req.body;
+    const { id } = req.params;
+    const { password } = req.body;
 
-  try {
-    const user = await getUserById(id);
-    const hashPassword = await bcrypt.hash(password, 10);
-    const updatedUser = await updateUser(user, hashPassword);
+    try {
+        const user = await getUserById(id);
+        const hashPassword = await bcrypt.hash(password, 10);
+        const updatedUser = await updateUser(user, hashPassword);
 
-    res.status(202).json(updatedUser);
-  } catch (err) {
-    res.status(500).json({ error: "Update was unsuccessful." });
-  }
+        res.status(202).json(updatedUser);
+    } catch (err) {
+        res.status(500).json({ error: "Update was unsuccessful." });
+    }
 });
 
 // DELETE
 user.delete("/:id", async (req, res) => {
-  const { id } = req.params;
-  try {
-    const deletedUser = await deleteUser(id);
-    res.status(200).json(deletedUser);
-  } catch (err) {
-    res.status(404).json({ error: "User was not found" });
-  }
+    const { id } = req.params;
+    try {
+        const deletedUser = await deleteUser(id);
+        res.status(200).json(deletedUser);
+    } catch (err) {
+        res.status(404).json({ error: "User was not found" });
+    }
 });
 
 module.exports = user;
--- frontend/src/Components/signUp.js
+++ frontend/src/Components/signUp.js
@@ -1,82 +1,79 @@
-import {useState} from 'react';
+import { useState } from "react";
 
 const API = process.env.REACT_APP_API_URL;
 
 function SignUp() {
+    const [user, setUser] = useState({
+        username: "",
+        email: "",
+        password: "",
+    });
 
-    const [user, setUser] = useState(
-        {
-            username: "", 
-            email: "", 
-            password: "",
-        }
-    );
-    
     const handleChange = (event) => {
-        setUser({...user, [event.target.id]: event.target.value});
+        setUser({ ...user, [event.target.id]: event.target.value });
     };
 
     const handleSubmit = async (event) => {
         event.preventDefault();
         try {
-            const response = await fetch(API+'/user/register', {
-            method: 'POST',
-            headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({user})
-        });
-          console.log("user", user);
-          const content = await response.json();
-          console.log("content", content);
+            const response = await fetch(`${API}/user/register`, {
+                method: "POST",
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(user),
+            });
+            console.log("user", user);
+            const content = await response.json();
+            console.log("content", content);
         } catch (error) {
             return error;
         }
     };
-    
-  return (
-    <form onSubmit={handleSubmit}>
-        <div className='txt_field'>
-            <label htmlFor='username'>Create Username:</label>
-            <input
-                type="text"
-                id="username"
-                name='username'
-                autoComplete='off'
-                onChange={handleChange}
-                value={user.userName}
-                required
-            />
-        </div>
-        <div className='txt_field'>
-            <label htmlFor='email'>Enter Email:</label>
-            <input
-                type="email"
-                id="email"
-                name='email'
-                autoComplete='off'
-                onChange={handleChange}
-                value={user.email}
-                required
-            />
-        </div>
-        <div className='txt_field'>
-            <label htmlFor='password'>Create Password:</label>
-            <input
-                type="password"
-                name='password'
-                id="password"
-                autoComplete='off'
-                onChange={handleChange}
-                value={user.password}
-                required
-            />
-        </div>
-        <div className='pass'>Forgot Password?</div>
-        <input type="submit" value="SignUp"/>
-    </form>
-  );
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className="txt_field">
+                <label htmlFor="username">Create Username:</label>
+                <input
+                    type="text"
+                    id="username"
+                    name="username"
+                    autoComplete="off"
+                    onChange={handleChange}
+                    value={user.userName}
+                    required
+                />
+            </div>
+            <div className="txt_field">
+                <label htmlFor="email">Enter Email:</label>
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    autoComplete="off"
+                    onChange={handleChange}
+                    value={user.email}
+                    required
+                />
+            </div>
+            <div className="txt_field">
+                <label htmlFor="password">Create Password:</label>
+                <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    autoComplete="off"
+                    onChange={handleChange}
+                    value={user.password}
+                    required
+                />
+            </div>
+            <div className="pass">Forgot Password?</div>
+            <input type="submit" value="SignUp" />
+        </form>
+    );
 }
 
 export default SignUp;
