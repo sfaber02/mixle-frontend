5adb1577f5aeb34e77b33aa07f91861125db84be
JoshMarte <joshuelm@gmail.com>
2022-05-20 10:47:57 -0400

    bs nav

--- frontend/src/Components/navBar.js
+++ frontend/src/Components/navBar.js
@@ -1,23 +1,84 @@
-import { Link } from "react-router-dom";
-import Dropdown from "./Dropdown";
-import "../Styles/Nav.css";
+// import { Link } from "react-router-dom";
+// import Dropdown from "./Dropdown";
+// import "../Styles/Nav.css";
 
 export default function NavBar() {
     return (
-        <div className="navbar-container">
-            <ul>
-                <li>
-                    <i class="fa-solid fa-circle-info fa-lg"></i>
-                </li>
-                <li>
-                    <Link to={"/"}>
-                        <h1>MIXLE</h1>
-                    </Link>
-                </li>
-                <li>
-                    <Dropdown />
-                </li>
-            </ul>
-        </div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">
+                Navbar
+            </a>
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">
+                            Home <span class="sr-only">(current)</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            Features
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            Pricing
+                        </a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a
+                            class="nav-link dropdown-toggle"
+                            href="#"
+                            id="navbarDropdownMenuLink"
+                            data-toggle="dropdown"
+                            aria-haspopup="true"
+                            aria-expanded="false"
+                        >
+                            Dropdown link
+                        </a>
+                        <div
+                            class="dropdown-menu"
+                            aria-labelledby="navbarDropdownMenuLink"
+                        >
+                            <a class="dropdown-item" href="#">
+                                Action
+                            </a>
+                            <a class="dropdown-item" href="#">
+                                Another action
+                            </a>
+                            <a class="dropdown-item" href="#">
+                                Something else here
+                            </a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        // <div className="navbar-container">
+        //     <ul>
+        //         <li>
+        //             <i class="fa-solid fa-circle-info fa-lg"></i>
+        //         </li>
+        //         <li>
+        //             <Link to={"/"}>
+        //                 <h1>MIXLE</h1>
+        //             </Link>
+        //         </li>
+        //         <li>
+        //             <Dropdown />
+        //         </li>
+        //     </ul>
+        // </div>
     );
 }
