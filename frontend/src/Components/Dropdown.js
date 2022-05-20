bd64dd7a99f104e78974487c4ebd908fb63771c7
JoshMarte <joshuelm@gmail.com>
2022-05-20 10:15:02 -0400

    navbar updates

--- frontend/src/Components/Dropdown.js
+++ frontend/src/Components/Dropdown.js
@@ -1,22 +1,33 @@
-export default function Dropdown (){
-    const handleClick = ()=>{
+import { useState } from "react";
 
-    }
-    const userId = localStorage.getItem("user_id")
-    if(userId){
+export default function Dropdown() {
+    const [drop, setDrop] = useState("dropdown-closed");
+
+    const handleClick = (event) => {
+        console.log(event.target);
+        if (drop === "dropdown-closed") {
+            setDrop("dropdown-open");
+        } else {
+            setDrop("dropdown-closed");
+        }
+    };
+    const userId = localStorage.getItem("user_id");
+    if (userId) {
         return (
             <div class="dropdown">
-                <button onClick={handleClick} className="dropbtn">{userId}</button>
-                <div id="myDropdown" class="dropdown-content">
+                <button onClick={handleClick} className="dropbtn">
+                    {userId}
+                    <i class="fa-solid fa-caret-down"></i>
+                </button>
+
+                <div id="myDropdown" class={drop}>
                     <a href="#home">My Mixes</a>
                     <a href="#about">View All Mixes</a>
                     <a href="#contact">Signout</a>
                 </div>
             </div>
-        )
+        );
     } else {
-        return (
-            <></>
-        )
+        return <></>;
     }
 }
--- frontend/src/Components/navBar.js
+++ frontend/src/Components/navBar.js
@@ -1,14 +1,23 @@
 import { Link } from "react-router-dom";
 import Dropdown from "./Dropdown";
+import "../Styles/Nav.css";
 
 export default function NavBar() {
-  return (
-    <div className="navbar-container">
-      <i class="fa-solid fa-circle-info"></i>
-      <Link to={"/"}>
-        <h1>MIXLE</h1>
-      </Link>
-      <Dropdown />
-    </div>
-  );
+    return (
+        <div className="navbar-container">
+            <ul>
+                <li>
+                    <i class="fa-solid fa-circle-info fa-lg"></i>
+                </li>
+                <li>
+                    <Link to={"/"}>
+                        <h1>MIXLE</h1>
+                    </Link>
+                </li>
+                <li>
+                    <Dropdown />
+                </li>
+            </ul>
+        </div>
+    );
 }
