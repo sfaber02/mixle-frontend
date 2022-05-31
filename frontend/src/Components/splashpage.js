4dd1eef0ff4878b49f8d5288a117731c1afead4e
JoshMarte <joshuelm@gmail.com>
2022-05-30 22:14:49 -0400

    open graph and splaph buttn

--- frontend/src/Components/splashpage.js
+++ frontend/src/Components/splashpage.js
@@ -2,26 +2,20 @@ import "../Styles/Splash.css";
 import "../Assets/Mixle_Icon-Nobg.png";
 
 /**
- * @param {object} props 
- * @param {function} handleStartClick function from mixer/splash wrapper component 
+ * @param {object} props
+ * @param {function} handleStartClick function from mixer/splash wrapper component
  * @returns splash page JSX
  */
 function SplashPage(props) {
-  return (
-    <div id="splashPageContainer">
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap"
-        rel="stylesheet"
-      />
-    
-      <div id="splashButtonContainer">
-        <button id="splashButton" onClick={props.handleStartClick}>
-          Start Mixing
-        </button>
-      </div>
-    </div>
-  );
+    return (
+        <div id="splashPageContainer">
+            <div id="splashButtonContainer">
+                <button id="splashButton" onClick={props.handleStartClick}>
+                    Start Mixing
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export { SplashPage };
