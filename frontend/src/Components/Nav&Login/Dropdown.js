4c73a76fbd5192568b665410f18ec82e48d8d183
Shawn Faber <96348445+sfaber02@users.noreply.github.com>
2022-06-03 16:20:11 -0400

    added delete user functionality

--- frontend/src/Components/Nav&Login/Dropdown.js
+++ frontend/src/Components/Nav&Login/Dropdown.js
@@ -5,12 +5,21 @@ import { useNavigate } from "react-router-dom";
  * @param {Object} user -  user object used to manipulate dropdown element
  * @returns JSX for dropdown element
  */
+
+const API = process.env.REACT_APP_API_URL;
+
 export default function Dropdown({ user }) {
     const navigate = useNavigate();
     const handleSignOut = () => {
         localStorage.clear();
         return navigate("/");
     };
+
+    const deleteUser = (args) => {
+        fetch(API + "/user/" + user.user_id, {
+            method: "DELETE",
+        }).then(handleSignOut)
+    }
 
     if (user.username) {
         return (
@@ -35,6 +44,9 @@ export default function Dropdown({ user }) {
                     <a className="dropdown-item" href="/">
                         My Mixes
                     </a>
+                    {/* <a className="dropdown-item" onClick={deleteUser} href="/">
+                        Delete 
+                    </a> */}
                     <a
                         className="dropdown-item"
                         href="/"
