98843db50d00402dbb9f9146de200a80b4f10a7b
Shawn Faber <96348445+sfaber02@users.noreply.github.com>
2022-06-17 22:32:53 -0400

    redid splash page css

--- frontend/src/Components/splashpage.js
+++ frontend/src/Components/splashpage.js
@@ -7,23 +7,23 @@ import "../Styles/Splash.css";
  * @returns splash page JSX
  */
 function SplashPage(props) {
-  return (
-    <section id="splashPageContainer">
-      <div className="top_row">
-        <img src={Image} alt="logo" className="logo" />
-      </div>
-      <div className="mid_row">
-        <div id="splashButtonContainer">
-          <button id="splashButton" onClick={props.handleStartClick}>
-            Start Mixing
-          </button>
-        </div>
-      </div>
-      <div className="low_row">
-        <h2>Music For Everyone</h2>
-      </div>
-    </section>
-  );
+    return (
+        <section id="splashPageContainer">
+            <div className="top_row">
+                <img src={Image} alt="logo" className="logo" />
+            </div>
+            <div className="mid_row">
+                <div id="splashButtonContainer">
+                    <button id="splashButton" onClick={props.handleStartClick}>
+                        Start Mixing
+                    </button>
+                </div>
+            </div>
+            <div className="low_row">
+                <h2>Music For Everyone</h2>
+            </div>
+        </section>
+    );
 }
 
 export { SplashPage };
